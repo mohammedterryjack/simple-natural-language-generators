90d3d95813ac6f995fac79b23532966623cd1d02
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-03-04 18:53:25 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
+from nltk.corpus import gutenberg
+
 from generators.dca_lma import DelayCoordinateEmbeddingNLG
 
-examples = [
-    "hi fahtima how are you ? ? ? ",
-    "hi whats up this is your dad ? ? ? ",
-    "hello family how are you ? ? ? ",
-    "hey what are you doing today ? ? ? "
-]
-x = DelayCoordinateEmbeddingNLG()
-x.train(sequences=examples)
+sentences = list(
+    map(
+        lambda sentence:' '.join(sentence),
+        gutenberg.sents('austen-persuasion.txt')
+    )
+)[:100]
 
-y = x.infer(sequence="hi fahtima how is")
+x = DelayCoordinateEmbeddingNLG(top_k=3)
+x.train(sequences=sentences)
+
+y = x.infer(sequence="then followed the history")
 print(y)
