4a41b5dfae152c3fe81a26bd2edf13d928b7c663
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-03-05 18:11:30 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from generators.dca_lma import DelayCoordinateEmbeddingNLG
 
 x = DelayCoordinateEmbeddingNLG(save_path="data")
-y = x.infer(sequence="stand up and")
+y = x.infer(sequence="i have never seen that")
 print(y)
